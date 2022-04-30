2b2a4e097a5dd5b9333c61f045d5fe2266226e1e
Brian Wise <brian_wise@icloud.com>
2022-04-30 12:43:21 +0300

    more cleanup

--- reverseP/go.mod
+++
@@ -1,5 +0,0 @@
-module godon
-
-go 1.17
-
-require github.com/bmf-san/godon v0.0.0-20220103095206-621a2dfc8750 // indirect
--- reverseP/main.go
+++ reverseP/main.go
@@ -1,7 +1,8 @@
-package godon
+package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
@@ -127,4 +128,18 @@ func Serve() {
 	if err = s.ListenAndServe(); err != nil {
 		log.Fatal(err.Error())
 	}
+}
+
+func serveBackend(name string, port string) {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Backend server name:%v\n", name)
+		fmt.Fprintf(w, "Response header:%v\n", r.Header)
+	}))
+	http.ListenAndServe(port, mux)
+}
+
+func main() {
+	Serve()
 }
